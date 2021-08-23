eb5328bc6b6d0e53aacc4f834cb5b4ee18223d8b
Dorian Edwards <de.pharmd2018@gmail.com>
2021-08-23 03:36:52 -0400

    add async error catch function

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,38 +1,36 @@
 const User = require('../models/user')
+const catchAsync = require('../utils/catchAsync')
+const AppError = require('../utils/appError')
 
-exports.fetchAll = async (req, res) => {
+exports.fetchAll = catchAsync(async (req, res) => {
   const users = await User.find()
   res
     .status(200)
     .json({ status: 'success', results: users.length, data: users })
-}
+})
 
-exports.create = async (req, res, next) => {
-  try {
-    const newUser = await User.create({ ...req.body })
-    res.status(201).json({ status: 'success', data: newUser })
-  } catch (err) {
-    next(err)
-  }
-}
+exports.create = catchAsync(async (req, res, next) => {
+  const newUser = await User.create({ ...req.body })
+  res.status(201).json({ status: 'success', data: newUser })
+})
 
-exports.fetchById = async (req, res) => {
+exports.fetchById = catchAsync(async (req, res) => {
   res.status(200).json({
     status: 'success',
     data: `Fetch single user id: ${req.params.id}`,
   })
-}
+})
 
-exports.update = async (req, res) => {
+exports.update = catchAsync(async (req, res) => {
   res
     .status(201)
     .json({ status: 'success', data: `Update user: ${req.params.id}` })
-}
+})
 
-exports.delete = async (req, res) => {
+exports.delete = catchAsync(async (req, res, next) => {
   const user = await User.findByIdAndDelete(req.params.id)
   if (!user) {
-    return res.status(400).json({ status: 'fail', message: 'User not found' })
+    return next(new AppError(`User not found`, 404))
   }
   res.status(200).json({ status: 'success', data: user })
-}
+})
