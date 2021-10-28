90eec41cce67e52027d6f2cdc8a1b26202115c1e
Dorian Edwards <de.pharmd2018@gmail.com>
2021-10-27 21:41:53 -0400

    slight mod to app to troubleshoot deployment

--- app.js
+++ app.js
@@ -28,13 +28,12 @@ app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }))
 
 app.use(express.static(path.join(__dirname, 'client', 'build')))
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+})
 app.use('/api/v1/', authRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/posts', postRouter)
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-})
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't ${req.method} ${req.originalUrl}`, 404))
 })
