acb31afe491d6ebc243c1dc03e5e1e12d921b5e4
Dorian Edwards <de.pharmd2018@gmail.com>
2021-10-27 23:38:28 -0400

    slight mod to app to troubleshoot deployment

--- app.js
+++ app.js
@@ -28,9 +28,9 @@ app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }))
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client', 'build')))
+  app.use(express.static('client/build')
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.join('client/build', 'index.html'))
   })
 }
 
