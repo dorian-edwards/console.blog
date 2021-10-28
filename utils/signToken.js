04c94cca10c0a102a87250179a8e4cf6480c1270
Dorian Edwards <de.pharmd2018@gmail.com>
2021-10-28 01:30:35 -0400

    modify cookie options

--- utils/signToken.js
+++ utils/signToken.js
@@ -9,6 +9,8 @@ const signToken = (res, user) => {
     expires: new Date(Date.now() + 3 * 60 * 60 * 1000),
     httpOnly: true,
     sameSite: 'lax',
+    secure: true,
+    domain: 'https://consoledotblog.netlify.app',
   })
 
   return token
