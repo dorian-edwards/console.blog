f20c739656b93052878f0d5b8b08944931702cc5
Dorian Edwards <de.pharmd2018@gmail.com>
2021-10-27 21:46:05 -0400

    slight mod to app to troubleshoot deployment

--- app.js
+++ app.js
@@ -27,10 +27,6 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }))
 
-app.use(express.static(path.join(__dirname, 'client', 'build')))
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-})
 app.use('/api/v1/', authRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/posts', postRouter)
