dbce16385ee929a24397a1a8015f082f83875280
Dorian Edwards <de.pharmd2018@gmail.com>
2021-10-28 09:34:25 -0400

    testing for deployment

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use('/api/v1/users', userRouter)
 app.use('/api/v1/posts', postRouter)
 
 // eslint-disable-next-line prefer-arrow-callback
-app.get('/*', function (req, res) {
+app.get('*', function (req, res) {
   const test = path.resolve(__dirname, './client/build', 'index.html')
   // res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
   res.send({ path: test })
